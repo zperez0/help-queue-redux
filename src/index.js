68c8d8ee20e4c3339c95992131b6936c079396e8
Yazmin Perez <zoeperez333@gmail.com>
2022-08-15 20:30:11 -0700

    add rootReducer

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
-import reducer from './reducers/ticket-list-reducer';
+// import reducer from './reducers/ticket-list-reducer';
 import { Provider } from 'react-redux';
+import rootReducer from './reducers/index';
 
-const store = createStore(reducer);
+const store = createStore(rootReducer);
+
+// const store = createStore(reducer);
 
 store.subscribe(() =>
   console.log(store.getState())
