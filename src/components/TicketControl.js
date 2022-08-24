aa25bbb84335739889b04eb13f4dd511e5dc9122
Yazmin Perez <zoeperez333@gmail.com>
2022-08-23 22:10:22 -0700

    WIP

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -18,6 +18,26 @@ class TicketControl extends React.Component {
     };
   }
   
+  componentDidMount() {
+    this.waitTimeUpdateTimer = setInterval(() =>
+      this.updateTicketElapsedWaitTime(),
+    1000
+    );
+  }
+
+  componentDidUpdate() {
+    console.log("component updated!");
+  }
+
+  componentWillUnmount(){
+    console.log("component unmounted!");
+    clearInterval(this.waitTimeUpdateTimer);
+  }
+
+  updateTicketElapsedWaitTime = () => {
+    console.log("tick");
+  }
+
   handleClick = () => {
     if (this.state.selectedTicket != null) {
       this.setState({
